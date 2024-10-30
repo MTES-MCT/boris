4130e865372a16be6ad7dea31987a0e886878438
benpaquier <hello@benoitpaquier.com>
2024-10-30 18:39:07 +0100

    feat: wrap content in Section component

--- client/src/routes/organismes-fonciers-solidaires/+page.svelte
+++ client/src/routes/organismes-fonciers-solidaires/+page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
+  import Section from '$lib/components/section/section.svelte';
   import type { Props } from './definitions';
 
   const { data }: Props = $props();
   const { ofsList } = data;
 </script>
 
-<h1>Organismes fonciers solidaires</h1>
-
-<ul>
-  {#each ofsList as ofs}
-    <li>{ofs.nom}</li>
-  {/each}
-</ul>
+<Section
+  title="Organismes fonciers solidaires"
+  titleElement="h1">
+  <ul>
+    {#each ofsList as ofs}
+      <li>{ofs.nom}, {ofs.region}, {ofs.departements}, {ofs.lien}</li>
+    {/each}
+  </ul>
+</Section>
