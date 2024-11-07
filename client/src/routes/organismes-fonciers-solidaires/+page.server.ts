14205e979239aa3ac6e1ce3e74ec884d9723b722
benpaquier <hello@benoitpaquier.com>
2024-11-07 17:11:48 +0100

    chore: use env variable for boris cms

--- client/src/routes/organismes-fonciers-solidaires/+page.server.ts
+++ client/src/routes/organismes-fonciers-solidaires/+page.server.ts
@@ -1,14 +1,13 @@
 // @ts-expect-error: no module
 import Papa from 'papaparse';
 import { formatOFSs } from '$lib/utils/formatters';
+import { BORIS_CMS_URL } from '$env/static/private';
 
 export const load = async () => {
-  const response = await fetch(
-    'https://boris-cms-test.osc-fr1.scalingo.io/api/v2/documents?title=liste-ofs',
-  );
+  const response = await fetch(`${BORIS_CMS_URL}/api/v2/documents`);
   const data = await response.json();
-  const document = data.items[0];
 
+  const document = data.items[0];
   const csvFile = await fetch(document.meta.download_url);
   const csvFileContent = await csvFile.text();
 
