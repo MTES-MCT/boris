3821e4c4cf4f63b6c69453127fe313bddf3d1742
benpaquier <hello@benoitpaquier.com>
2024-11-27 22:56:49 +0100

    feat: statistiques page

--- client/src/routes/statistiques/+page.svelte
+++ client/src/routes/statistiques/+page.svelte
@@ -9,7 +9,24 @@
 <Section
   title="Statistiques"
   titleElement="h1">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia eos sed
-  architecto aliquid quasi! Amet laudantium ut accusamus, saepe repellat rerum
-  natus tempora, aliquam enim excepturi dolorem eaque quam possimus!
+  <h2>Nos impacts chiffrés du 1e mars au 30 juin 2024</h2>
+
+  <p class="fr-text--lead fr-mb-0"><b>600 simulations</b></p>
+  <p>dont 87% concernent des ménages éligibles dans toute la France.</p>
+
+  <p class="fr-text--lead fr-mb-0">
+    <b>200 mises en relation avec des ménages se déclarant intéressés</b>
+  </p>
+  <p>
+    dont 66% qui ne connaissaient pas le BRS avant (obj : 33%).
+    <br />
+    45% en région PACA / 20% en région IDF / 10% dans le 69 et le 74 / 25% dilués
+    dans le reste de la France
+  </p>
+
+  <p class="fr-text--lead"><b>90 échanges téléphonique</b></p>
+
+  <p class="fr-text--lead fr-mb-0">
+    <b>20 ménages en chemin vers l’accession</b>
+  </p>
 </Section>
