4518b64b18730715025bd31a9973ea0675c3df08
benpaquier <hello@benoitpaquier.com>
2024-12-03 17:26:18 +0100

    fix: deploy

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -1,6 +1,6 @@
 import { sequence } from '@sveltejs/kit/hooks';
 import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
-import * as Sentry from '@sentry/sveltekit';
+import Sentry from '@sentry/sveltekit';
 
 Sentry.init({
   dsn: 'https://a08d74bb41ceec5e916f126744df60ea@sentry.incubateur.net/207',
