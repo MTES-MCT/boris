48e33eb390c50a2f8fd95e15e2ddca9a7c649db5
benpaquier <hello@benoitpaquier.com>
2024-12-05 14:57:39 +0100

    fix: deploy

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -1,6 +1,5 @@
-import { sequence } from '@sveltejs/kit/hooks';
-import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
-import Sentry from '@sentry/sveltekit';
+import * as Sentry from '@sentry/node';
+import type { HandleServerError } from '@sveltejs/kit';
 
 Sentry.init({
   dsn: 'https://a08d74bb41ceec5e916f126744df60ea@sentry.incubateur.net/207',
@@ -11,8 +10,20 @@ Sentry.init({
   // spotlight: import.meta.env.DEV,
 });
 
-// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-export const handle = sequence(sentryHandle());
+export const handleError: HandleServerError = async ({
+  error,
+  event,
+  status,
+}) => {
+  const errorId = crypto.randomUUID();
 
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry();
+  // example integration with https://sentry.io/
+  Sentry.captureException(error, {
+    extra: { event, errorId, status },
+  });
+
+  return {
+    message: 'Whoops!',
+    errorId,
+  };
+};
