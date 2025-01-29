5fffcecb0c9bb8592910db49d85fe09a1e180de1
benpaquier <hello@benoitpaquier.com>
2025-01-29 14:40:55 +0100

    chore: sentry

---
+++ client/src/hooks.server.ts
@@ -0,0 +1,18 @@
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: 'https://a08d74bb41ceec5e916f126744df60ea@sentry.incubateur.net/207',
+
+  tracesSampleRate: 1.0,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: import.meta.env.DEV,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,18 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sentrySvelteKit({
+      sourceMapsUploadOptions: {
+        org: 'boris',
+        project: 'client',
+        url: 'https://sentry.incubateur.net/',
+      },
+    }),
+    sveltekit(),
+  ],
   test: {
     include: ['tests/unit/**/*.{test,spec}.{js,ts}'],
   },
