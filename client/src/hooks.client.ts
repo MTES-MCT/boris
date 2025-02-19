b340aaaecc7bb9b8c8c88f6d274da5f8f991429b
fuuuzz <benoit.paquier@fairness.coop>
2025-02-19 12:45:21 +0100

    chore: disable sentry dev env

--- client/src/hooks.client.ts
+++ client/src/hooks.client.ts
@@ -1,10 +1,13 @@
 import { handleErrorWithSentry } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
+console.log(location.host.includes('localhost'));
+
 Sentry.init({
   dsn: 'https://a08d74bb41ceec5e916f126744df60ea@sentry.incubateur.net/207',
 
   tracesSampleRate: 1.0,
+  enabled: !location.host.includes('localhost'),
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
