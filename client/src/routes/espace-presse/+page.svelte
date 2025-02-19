244dde6616df34b2dc16829751c89b54a146d0b7
fuuuzz <benoit.paquier@fairness.coop>
2025-02-19 10:47:28 +0100

    feat: create espace-presse page

---
+++ client/src/routes/espace-presse/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import GradientBackgroundWrapper from '$components/common/GradientBackgroundWrapper.svelte';
+  import Section from '$components/common/Section.svelte';
+</script>
+
+<GradientBackgroundWrapper>
+  <Section
+    title="Espace presse"
+    titleElement="h1">
+    <p>
+      Nous avons regroupé sur cette page l’essentiel des informations relatives
+      à BoRiS. Pour toute information complémentaire ou demande d’interview,
+      merci de nous contacter <a href="/nous-contacter"><b>ici</b></a>
+      .
+    </p>
+
+    <a
+      href="https://drive.google.com/drive/folders/1ILzjig74CXB7R57gq0wzrKrzLd-xVYxi"
+      target="_blank"
+      rel="noopener">
+      <b>Dossier de presse</b>
+    </a>
+  </Section>
+</GradientBackgroundWrapper>
