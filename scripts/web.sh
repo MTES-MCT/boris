1d63736837f691c71737d5dc93ca727c652ee8eb
fuuuzz <benoit.paquier@fairness.coop>
2025-02-20 15:15:30 +0100

    fix: default yarn.lock

--- scripts/web.sh
+++ scripts/web.sh
@@ -3,6 +3,5 @@ echo "$APP_NAME: Procfile > web"
 if [ "$APP_NAME" = "frontend" ] ; then
   npm run start -w @boris/frontend
 elif [ "$APP_NAME" = "backend" ]; then
-  npm i --save-dev @types/node -w @boris/backend &&
 	npm run start -w @boris/backend
 fi
