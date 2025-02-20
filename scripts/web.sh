bafdb97fa732e06ebe784c8586a33f3e86c3093f
fuuuzz <benoit.paquier@fairness.coop>
2025-02-20 16:55:43 +0100

    chore: move frontend to apps directory

--- scripts/web.sh
+++ scripts/web.sh
@@ -1,7 +1,7 @@
 echo "$APP_NAME: Procfile > web"
 
 if [ "$APP_NAME" = "frontend" ] ; then
-  npm run start -w @boris/frontend
+  npm run start -w @boris/client
 elif [ "$APP_NAME" = "backend" ]; then
 	npm run start -w @boris/backend
 fi
