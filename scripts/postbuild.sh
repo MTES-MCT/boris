a9c08a45f1a18bf50796fe2a1ed7ff892c507a51
fuuuzz <benoit.paquier@fairness.coop>
2025-02-20 15:44:31 +0100

    deloy: test

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -3,6 +3,5 @@ echo "$APP_NAME: package.json > scalingo-postbuild"
 if [ "$APP_NAME" = "frontend" ] ; then
   npm run build -w @boris/client
 elif [ "$APP_NAME" = "backend" ] ; then
-  npm i -g @nestjs/cli
   npm run build -w @boris/backend
 fi
