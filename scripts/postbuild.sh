33420130d7c89c6a197b0d74116ce5ced369ee11
fuuuzz <benoit.paquier@fairness.coop>
2025-02-20 12:07:10 +0100

    chore: backend scripts for deployment

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -2,4 +2,6 @@ echo "$APP_NAME: package.json > scalingo-postbuild"
 
 if [ "$APP_NAME" = "frontend" ] ; then
   npm run build -w client
+elif [ "$APP_NAME" = "backend" ] ; then
+  npm run build -w apps/backend
 fi
--- scripts/web.sh
+++ scripts/web.sh
@@ -2,4 +2,6 @@ echo "$APP_NAME: Procfile > web"
 
 if [ "$APP_NAME" = "frontend" ] ; then
   npm run start -w client
+elif [ "$APP_NAME" = "backend" ]; then
+	npm run start -w apps/backend
 fi
