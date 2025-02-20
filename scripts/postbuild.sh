4288bbba2be1d93b1f0678cf14fc1204bd39c70e
fuuuzz <benoit.paquier@fairness.coop>
2025-02-20 14:35:22 +0100

    chore: globally install nest after build

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -3,5 +3,6 @@ echo "$APP_NAME: package.json > scalingo-postbuild"
 if [ "$APP_NAME" = "frontend" ] ; then
   npm run build -w @boris/client
 elif [ "$APP_NAME" = "backend" ] ; then
+  npm i -g @nestjs/cli
   npm run build -w @boris/backend
 fi
