8ff12cb1f9f023ed6f491257be6866911050370a
fuuuzz <benoit.paquier@fairness.coop>
2025-02-20 15:06:21 +0100

    chore: backend tsconfig

--- scripts/web.sh
+++ scripts/web.sh
@@ -3,6 +3,6 @@ echo "$APP_NAME: Procfile > web"
 if [ "$APP_NAME" = "frontend" ] ; then
   npm run start -w @boris/frontend
 elif [ "$APP_NAME" = "backend" ]; then
-  npm i --save-dev @types/node -w @boris/backend
+  npm i --save-dev @types/node -w @boris/backend &&
 	npm run start -w @boris/backend
 fi
