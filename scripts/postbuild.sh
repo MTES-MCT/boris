4a2ecb47b63b3a86eb6316e7add32dbd860eb27c
fuuuzz <benoit.paquier@fairness.coop>
2025-02-20 14:12:20 +0100

    chore: change workspaces in deploy scripts

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,7 +1,7 @@
 echo "$APP_NAME: package.json > scalingo-postbuild"
 
 if [ "$APP_NAME" = "frontend" ] ; then
-  npm run build -w client
+  npm run build -w @boris/client
 elif [ "$APP_NAME" = "backend" ] ; then
-  npm run build -w apps/backend
+  npm run build -w @boris/backend
 fi
