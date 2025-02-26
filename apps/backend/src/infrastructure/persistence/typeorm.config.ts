62f2bfb2f9b9e0fc87bce9527e19089478732398
fuuuzz <benoit.paquier@fairness.coop>
2025-02-26 16:19:47 +0100

    oops: remove logs

--- apps/backend/src/infrastructure/persistence/typeorm.config.ts
+++ apps/backend/src/infrastructure/persistence/typeorm.config.ts
@@ -4,14 +4,6 @@ dotenv.config();
 
 const test = process.env.NODE_ENV === 'test';
 
-console.log({
-  host: process.env.POSTGRES_HOST,
-  port: !test ? +(process.env.POSTGRES_PORT || '5432') : 5433,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: !test ? process.env.POSTGRES_DB : `${process.env.POSTGRES_DB}-test`,
-});
-
 export const typeormConfig: DataSourceOptions = {
   type: 'postgres',
   url: process.env.POSTGRES_URL,
