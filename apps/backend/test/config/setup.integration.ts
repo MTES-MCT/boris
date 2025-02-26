09de80bd4d5b72b100510d8c449869adfa0c99d2
fuuuzz <benoit.paquier@fairness.coop>
2025-02-26 14:51:40 +0100

    specs: add backend e2e test in ci

--- apps/backend/test/config/setup.integration.ts
+++ apps/backend/test/config/setup.integration.ts
@@ -1,12 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 
-import { Test, TestingModule } from '@nestjs/testing';
+// import { Test, TestingModule } from '@nestjs/testing';
 // import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppModule } from 'src/app.module';
+// import { AppModule } from 'src/app.module';
 // import { bretagne } from 'test/mocks/region';
 // import { DataSource, QueryRunner } from 'typeorm';
 
@@ -46,32 +43,32 @@ global.beforeEach(() => {
 // global.afterAll(async () => {
 //   await queryRunner.manager.query(`DELETE FROM region`);
 //   await appDataSource.destroy();
-//   await global.app?.close();
+// await global.app?.close();
 // });
 
-global.setAppModule = async (
-  entity,
-  useCase,
-  repository,
-  repositoryInterface: string,
-  controller,
-) => {
-  const module: TestingModule = await Test.createTestingModule({
-    imports: [
-      // TypeOrmModule.forRoot({
-      //   ...dbInfo,
-      //   entities: [entity],
-      // }),
-      // TypeOrmModule.forFeature([entity]),
-      AppModule,
-    ],
-    providers: [
-      useCase,
-      { provide: repositoryInterface, useClass: repository },
-    ],
-    controllers: [controller],
-  }).compile();
+// global.setAppModule = async (
+//   entity,
+//   useCase,
+//   repository,
+//   repositoryInterface: string,
+//   controller,
+// ) => {
+//   const module: TestingModule = await Test.createTestingModule({
+//     imports: [
+//       // TypeOrmModule.forRoot({
+//       //   ...dbInfo,
+//       //   entities: [entity],
+//       // }),
+//       // TypeOrmModule.forFeature([entity]),
+//       AppModule,
+//     ],
+//     providers: [
+//       useCase,
+//       { provide: repositoryInterface, useClass: repository },
+//     ],
+//     controllers: [controller],
+//   }).compile();
 
-  global.app = module.createNestApplication();
-  global.app.init();
-};
+//   global.app = module.createNestApplication();
+//   global.app.init();
+// };
