559e760d9ce77da9a7f811afbcd1f549028ad507
fuuuzz <benoit.paquier@ik.me>
2025-04-29 11:35:03 +0200

    specs: unit tests on ofs entity

--- apps/backend/test/mocks/departement.ts
+++ apps/backend/test/mocks/departement.ts
@@ -1,7 +1,8 @@
 import { DepartementEntity } from 'src/infrastructure/departement/departement.entity';
+import { bretagne } from './region';
 
-export const finistere = new DepartementEntity('Finistère', '29');
-export const paris = new DepartementEntity('Paris', '75');
+export const finistere = new DepartementEntity('Finistère', '29', bretagne);
+export const paris = new DepartementEntity('Paris', '75', bretagne);
 
 export const mockDepartementRepository = {
   save: jest.fn(),
---
+++ apps/backend/test/mocks/ofs.ts
@@ -0,0 +1,11 @@
+import { OfsEntity } from 'src/infrastructure/ofs/ofs.entity';
+import { finistere, paris } from './departement';
+import { bretagne } from './region';
+
+export const ofs1 = new OfsEntity(
+  'La foncière de Bretagne',
+  '0203040506',
+  'https://boris.beta.gouv.fr',
+  [finistere, paris],
+  [bretagne],
+);
