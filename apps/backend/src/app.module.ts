3f2826b365d124230f17f90a8c962bdf352a5eab
fuuuzz <benoit.paquier@ik.me>
2025-04-29 11:20:57 +0200

    feat: ofs entity

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeormConfig } from './infrastructure/persistence/typeorm.config';
 import { RegionModule } from './infrastructure/region/region.module';
+import { OfsModule } from './infrastructure/ofs/ofs.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig), RegionModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), RegionModule, OfsModule],
   controllers: [AppController],
   providers: [AppService],
 })
