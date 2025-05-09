c48b2067b39db2cc369bb6faa7f12f6345373842
fuuuzz <benoit.paquier@ik.me>
2025-05-09 15:38:57 +0200

    feat: add pagination

---
+++ apps/backend/src/application/pagination/pagination.ts
@@ -0,0 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray } from 'class-validator';
+import { PaginationProps } from 'src/domain/pagination/paginationProps';
+
+export class Pagination<T> {
+  @IsArray()
+  @ApiProperty({ isArray: true })
+  public items: T[];
+
+  @ApiProperty()
+  public readonly totalCount: number;
+
+  @ApiProperty()
+  public readonly page: number;
+
+  @ApiProperty()
+  public readonly pageSize: number;
+
+  @ApiProperty()
+  public readonly pagesCount: number;
+
+  @ApiProperty()
+  public readonly hasPreviousPage: boolean;
+
+  @ApiProperty()
+  public readonly hasNextPage: boolean;
+
+  constructor(
+    items: T[],
+    totalCount: number,
+    paginationProps: PaginationProps,
+  ) {
+    this.items = items;
+    this.totalCount = totalCount;
+    this.page = paginationProps.page;
+    this.pageSize = paginationProps.pageSize;
+    this.pagesCount = Math.ceil(this.totalCount / this.pageSize);
+    this.hasPreviousPage = this.page > 1;
+    this.hasNextPage = this.page < this.pagesCount;
+  }
+}
---
+++ apps/backend/test/infrastructure/pagination/pagination.dto.unit-spec.ts
@@ -0,0 +1,45 @@
+import { validate } from 'class-validator';
+import { PaginationDTO } from 'src/infrastructure/pagination/pagination.dto';
+
+describe('PaginationDTO', () => {
+  it('should be valid', async () => {
+    const dto = new PaginationDTO();
+    dto.page = 1;
+
+    const validation = await validate(dto);
+    expect(validation).toHaveLength(0);
+  });
+
+  it('should fail when page is smaller than 1', async () => {
+    const dto = new PaginationDTO();
+    dto.page = -1;
+
+    const validation = await validate(dto);
+    expect(validation).toHaveLength(1);
+    expect(validation[0].constraints).toMatchObject({
+      min: 'page must not be less than 1',
+    });
+  });
+
+  it('should fail when pageSize is smaller than 1', async () => {
+    const dto = new PaginationDTO();
+    dto.pageSize = -1;
+
+    const validation = await validate(dto);
+    expect(validation).toHaveLength(1);
+    expect(validation[0].constraints).toMatchObject({
+      min: 'pageSize must not be less than 1',
+    });
+  });
+
+  it('should fail when pageSize is greater than 50', async () => {
+    const dto = new PaginationDTO();
+    dto.pageSize = 99;
+
+    const validation = await validate(dto);
+    expect(validation).toHaveLength(1);
+    expect(validation[0].constraints).toMatchObject({
+      max: 'pageSize must not be greater than 50',
+    });
+  });
+});
