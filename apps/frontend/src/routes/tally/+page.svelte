b2f555296c3da7f3597cfaf1cc2d7b6d6a8f33cf
fuuuzz <benoit.paquier@ik.me>
2025-06-03 11:47:04 +0200

    feat: initialize tally page

---
+++ apps/frontend/src/routes/tally/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Section from '$components/common/Section.svelte';
+</script>
+
+<svelte:head>
+  <title>Questionnaire | BORIS</title>
+  <meta
+    name="robots"
+    content="noindex, nofollow" />
+</svelte:head>
+
+<Section>
+  <h1>Questionnaire</h1>
+  <p>Contenu du questionnaire à venir...</p>
+</Section>
