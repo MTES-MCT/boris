488ff6343408d41629c83b760ae3a87ab2afc686
fuuuzz <benoit.paquier@ik.me>
2025-06-05 16:54:39 +0200

    cleanup: infra layer

--- apps/backend/src/application/auth/usecases/login.usecase.ts
+++ apps/backend/src/application/auth/usecases/login.usecase.ts
@@ -10,9 +10,10 @@ export class LoginUsecase {
     private readonly userRepository: UserRepositoryInterface,
   ) {}
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public execute({ email, password }: LoginParams): UserView {
     const user = this.userRepository.findOneByEmail(email.toLowerCase());
-    console.log(password);
+    // console.log(password);
 
     if (!user) {
       throw new UnauthorizedException('Invalid credentials');
