def7f765cf3cbf4adedf2ea58d2ed59fe703d070
fuuuzz <benoit.paquier@ik.me>
2025-08-11 11:00:17 +0200

    feat: paginated search

--- apps/frontend/src/lib/api/brs-diffusion-websites.ts
+++ apps/frontend/src/lib/api/brs-diffusion-websites.ts
@@ -1,10 +1,21 @@
 import { API_URL } from '$env/static/private';
 import type { Pagination, BrsDiffusionWebsiteView } from '$lib/utils/api-types';
+import type { operations } from '$lib/utils/generated-api-types';
 
-export const getBrsDiffusionWebsites = async (): Promise<
-  Pagination<BrsDiffusionWebsiteView>
-> => {
-  const response = await fetch(`${API_URL}/brs-diffusion-websites`);
+export const getBrsDiffusionWebsites = async (
+  query?: operations['GetBrsDiffusionWebsitesApiController_index']['parameters']['query'],
+): Promise<Pagination<BrsDiffusionWebsiteView>> => {
+  const url = new URL(`${API_URL}/brs-diffusion-websites`);
+
+  if (query?.page) {
+    url.searchParams.set('page', query.page.toString());
+  }
+
+  if (query?.pageSize) {
+    url.searchParams.set('pageSize', query.pageSize.toString());
+  }
+
+  const response = await fetch(url);
   const data: Pagination<BrsDiffusionWebsiteView> = await response.json();
 
   return data;
