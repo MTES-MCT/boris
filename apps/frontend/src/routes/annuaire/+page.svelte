098f1564611888aa5817832691e1388f535bc9fb
fuuuzz <benoit.paquier@ik.me>
2025-08-11 11:47:56 +0200

    feat: initialize pagination

--- apps/frontend/src/routes/annuaire/+page.svelte
+++ apps/frontend/src/routes/annuaire/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import GradientBackgroundWrapper from '$components/common/GradientBackgroundWrapper.svelte';
+  import Pagination from '$components/common/Pagination.svelte';
   import Section from '$components/common/Section.svelte';
   import Card from '$components/pages/annuaire/Card.svelte';
   import type { DataType } from './+page.server';
@@ -13,6 +14,10 @@
 
 <GradientBackgroundWrapper>
   <Section title="Annuaire des sites de diffusion du BRS">
+    <Pagination
+      {...data.brsDiffusionWebsites}
+      baseUrl="/annuaire" />
+
     <div class="fr-container--fluid">
       <div class="fr-grid-row fr-grid-row--gutters">
         {#each data.brsDiffusionWebsites.items as item}
