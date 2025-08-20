35a766e728a2c0612096024f5252952516bbeada
fuuuzz <benoit.paquier@ik.me>
2025-08-20 11:01:13 +0200

    ci: fix lighthouse

--- apps/frontend/src/lib/utils/helpers.ts
+++ apps/frontend/src/lib/utils/helpers.ts
@@ -3,6 +3,8 @@ import { PUBLIC_NODE_ENV } from '$env/static/public';
 export const blockSearchEngineIndexing = (page: Page): boolean => {
   const hiddenPaths = ['/questionnaire', '/simulateur-acquisition'];
 
+  console.log('PUBLIC_NODE_ENV', PUBLIC_NODE_ENV);
+
   return (
     (PUBLIC_NODE_ENV !== 'production' && PUBLIC_NODE_ENV !== 'ci') ||
     hiddenPaths.includes(page.route.id as string)
