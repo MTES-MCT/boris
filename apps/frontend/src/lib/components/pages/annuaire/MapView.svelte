c442d601c3343430c9d26a2f0199fd18af976522
fuuuzz <benoit.paquier@ik.me>
2025-08-28 15:52:45 +0200

    feat: show all markers on map events

--- apps/frontend/src/lib/components/pages/annuaire/MapView.svelte
+++ apps/frontend/src/lib/components/pages/annuaire/MapView.svelte
@@ -9,6 +9,7 @@
   import annuaireManager from '$lib/managers/annuaire.svelte';
   import { defaultZoom } from '$lib/utils/constants';
   import { debounce } from '$lib/utils/helpers';
+  import { getBrsDiffusionWebsitesByBounds } from '$lib/api/brs-diffusion-websites';
 
   type Props = {
     brsDiffusionWebsites: PaginationType<BrsDiffusionWebsiteView>;
@@ -30,9 +31,7 @@
 
       if (map) {
         deleteMarkersFromMap();
-        addMarkersToMap(
-          annuaireManager.brsDiffusionWebsites as PaginationType<BrsDiffusionWebsiteView>,
-        );
+        addMarkersToMap(annuaireManager.brsDiffusionWebsites.items);
       }
     }
   });
@@ -48,7 +47,7 @@
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    addMarkersToMap(brsDiffusionWebsites);
+    addMarkersToMap(brsDiffusionWebsites.items);
 
     map.on('zoomlevelschange', debounce(handleMapBoundsChange, 500));
     map.on('moveend', debounce(handleMapBoundsChange, 500));
@@ -62,18 +61,46 @@
     markers = [];
   };
 
-  const addMarkersToMap = (
-    brsDiffusionWebsites: PaginationType<BrsDiffusionWebsiteView>,
-  ) => {
-    brsDiffusionWebsites?.items.forEach((item) => {
+  const addMarkersToMap = (brsDiffusionWebsites: BrsDiffusionWebsiteView[]) => {
+    brsDiffusionWebsites?.forEach((item) => {
       markers.push(L.marker([item.latitude, item.longitude]));
     });
 
     markers.forEach((marker) => marker.addTo(map as Map));
   };
 
-  const handleMapBoundsChange = () => {
-    console.log(map?.getBounds());
+  const handleMapBoundsChange = async () => {
+    const data = await getBrsDiffusionWebsitesByBounds({
+      northEastLat: map?.getBounds().getNorthEast().lat as number,
+      northEastLng: map?.getBounds().getNorthEast().lng as number,
+      southWestLat: map?.getBounds().getSouthWest().lat as number,
+      southWestLng: map?.getBounds().getSouthWest().lng as number,
+    });
+
+    let items = [...data.items];
+
+    let currentPage = data.page;
+
+    if (data.hasNextPage) {
+      for (const pageNumber of new Array(data.pagesCount - 1)) {
+        currentPage = currentPage + 1;
+
+        const pageData = await getBrsDiffusionWebsitesByBounds({
+          page: currentPage,
+          northEastLat: map?.getBounds().getNorthEast().lat as number,
+          northEastLng: map?.getBounds().getNorthEast().lng as number,
+          southWestLat: map?.getBounds().getSouthWest().lat as number,
+          southWestLng: map?.getBounds().getSouthWest().lng as number,
+        });
+
+        items = [...items, ...pageData.items];
+
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+    }
+
+    deleteMarkersFromMap();
+    addMarkersToMap(items);
   };
 
   $inspect(annuaireManager.brsDiffusionWebsites);
