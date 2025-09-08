d640cec18fa2db03ddd1ec5b68c54d5444225ff2
fuuuzz <benoit.paquier@ik.me>
2025-09-08 14:04:39 +0200

    feat: tweaks

--- apps/backend/src/instrument.ts
+++ apps/backend/src/instrument.ts
@@ -1,5 +1,6 @@
 import * as Sentry from '@sentry/nestjs';
 
 Sentry.init({
-  dsn: 'https://5e6ddcd2de5fb70eb855f8c44b24074c@sentry.incubateur.net/260',
+  dsn: process.env.SENTRY_DSN,
+  enabled: process.env.NODE_ENV !== 'development',
 });
