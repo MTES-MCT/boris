548e7662e4abc6d1ac722b6ca66645207b131037
fuuuzz <benoit.paquier@ik.me>
2025-09-08 17:01:35 +0200

    feat: use in memory cache

--- apps/frontend/src/routes/logements-brs-disponibles/+page.server.ts
+++ apps/frontend/src/routes/logements-brs-disponibles/+page.server.ts
@@ -1,15 +1,31 @@
 import { API_URL } from '$env/static/private';
 import { formatOfss } from '$lib/utils/formatters';
-
 import type { OfsView, Pagination } from '$lib/utils/api-types';
+import Keyv from 'keyv';
+
+const TTL_MINS = 1;
+const TTL_MS = TTL_MINS * 60 * 1000;
+const ofssCache = new Keyv({ namespace: 'ofss', ttl: TTL_MS });
 
 export const load = async () => {
-  const response = await fetch(`${API_URL}/ofss`);
-  const ofss: Pagination<OfsView> = await response.json();
+  const cache = await ofssCache.get('ofss');
 
-  return {
-    regions: formatOfss(ofss.items),
-  };
+  console.log(cache);
+
+  if (cache) {
+    return {
+      regions: formatOfss(cache.items),
+    };
+  } else {
+    const response = await fetch(`${API_URL}/ofss`);
+    const ofss: Pagination<OfsView> = await response.json();
+
+    ofssCache.set('ofss', ofss);
+
+    return {
+      regions: formatOfss(ofss.items),
+    };
+  }
 };
 
 export const prerender = false;
