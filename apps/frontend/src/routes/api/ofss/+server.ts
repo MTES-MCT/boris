8889e648ca5187dd863ad70b376aaebcedb428df
fuuuzz <benoit.paquier@ik.me>
2025-09-10 12:22:17 +0200

    feat: use api key on frontend

--- apps/frontend/src/routes/api/ofss/+server.ts
+++ apps/frontend/src/routes/api/ofss/+server.ts
@@ -1,4 +1,4 @@
-import { API_URL } from '$env/static/private';
+import { API_URL, API_KEY } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 import cache, { namespaces } from '$lib/server/cache';
 
@@ -6,7 +6,11 @@ export const GET = async () => {
   let ofss = await cache.get(namespaces.ofss);
 
   if (!ofss) {
-    const response = await fetch(`${API_URL}/ofss`);
+    const response = await fetch(`${API_URL}/ofss`, {
+      headers: {
+        'x-api-key': API_KEY,
+      },
+    });
     ofss = await response.json();
     cache.set(namespaces.ofss, ofss);
   }
