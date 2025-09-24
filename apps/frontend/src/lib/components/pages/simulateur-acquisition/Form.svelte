a8bb33d77ef7f1c0b9d7335dd4a495cfd6c11cc9
fuuuzz <benoit.paquier@ik.me>
2025-09-24 17:26:07 +0200

    feat: step 1 form

---
+++ apps/frontend/src/lib/components/pages/simulateur-acquisition/Form.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import '@gouvfr/dsfr/dist/component/form/form.min.css';
+
+  import type { Snippet } from 'svelte';
+
+  type Props = {
+    onSubmit: () => void;
+    children: Snippet;
+  };
+
+  const { onSubmit, children }: Props = $props();
+</script>
+
+<form
+  class="fr-my-6w"
+  autocomplete="off"
+  onsubmit={onSubmit}>
+  {@render children()}
+</form>
+
+<style lang="postcss">
+  form {
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 2rem;
+    border-radius: var(--border-radius-lg);
+    box-shadow: 0 6px 12px rgba(134, 144, 162, 0.3);
+    border: solid 1px rgba(134, 144, 162, 0.1);
+  }
+</style>
