612e85b323a1f0e7fed20fc8ef1c1c4596125e18
fuuuzz <benoit.paquier@ik.me>
2025-09-29 17:53:21 +0200

    feat: step 3

---
+++ apps/frontend/src/lib/components/pages/simulateur-acquisition/Description.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  import Stepper from '$components/common/Stepper.svelte';
+
+  import acquisitionSimulatorManger from '$lib/managers/acquisition-simulator.svelte';
+
+  let { steps, currentStep, nextStep } = $derived(acquisitionSimulatorManger);
+
+  type Props = {
+    content: string;
+  };
+
+  const { content }: Props = $props();
+</script>
+
+<div class="fr-col-7 fr-pr-10w">
+  <Stepper
+    title={`${currentStep.step}. ${currentStep.title}`}
+    nextStepTitle={nextStep?.title}
+    currentStep={currentStep.step}
+    stepCount={steps.length} />
+  <p>
+    {content}
+  </p>
+</div>
--- apps/frontend/src/lib/components/pages/simulateur-acquisition/Step2.svelte
+++ apps/frontend/src/lib/components/pages/simulateur-acquisition/Step2.svelte
@@ -2,6 +2,7 @@
   import z, { ZodError } from 'zod';
 
   import type { FormFieldError } from '$lib/utils/definitions';
+  import { formatFormErrors } from '$lib/utils/helpers';
 
   import Form from '$components/pages/simulateur-acquisition/Form.svelte';
   import Input from '$components/common/Input.svelte';
@@ -9,7 +10,8 @@
   import acquisitionSimulatorManger from '$lib/managers/acquisition-simulator.svelte';
   import Actions from '$components/pages/simulateur-acquisition/Actions.svelte';
   import Action from '$components/pages/simulateur-acquisition/Action.svelte';
-  import { formatFormErrors } from '$lib/utils/helpers';
+  import Wrapper from '$components/pages/simulateur-acquisition/Wrapper.svelte';
+  import Description from './Description.svelte';
 
   let { ownContribution, nextStep, previousStep, goToPreviousStep } = $derived(
     acquisitionSimulatorManger,
@@ -20,7 +22,8 @@
   const FormData = z.object({
     ownContribution: z
       .number()
-      .gte(0, 'Veuillez saisir un chiffre supérieur à 0.'),
+      .gte(0, 'Veuillez saisir un chiffre supérieur à 0.')
+      .optional(),
   });
 
   const handleSubmit = (e: SubmitEvent) => {
@@ -38,38 +41,36 @@
       errors = formatFormErrors((e as ZodError).issues);
     }
   };
-
-  $inspect(ownContribution);
 </script>
 
-<p>
-  Renseignez le montant de votre apport personnel. Cela nous permet de calculer
-  la part du financement que vous pouvez couvrir sans emprunter, et d'ajuster le
-  besoin de crédit à solliciter.
-</p>
-<Form onSubmit={handleSubmit}>
-  <div class="fieldset-container">
-    <fieldset class="fr-fieldset">
-      <div class="fr-fieldset__element fr-mb-4w">
-        <Input
-          value={ownContribution}
-          label="Montant de votre apport (€)"
-          labelTooltip="Somme dont vous disposez pour le projet (épargne, donation,
-              aide familiale, etc.)."
-          id="own-contribution"
-          error={errors.ownContribution}
-          type="number"
-          placeholder="10 000€"
-          onChange={(e) => {
-            acquisitionSimulatorManger.ownContribution = Number(
-              (e.target as HTMLInputElement).value,
-            );
-          }} />
-      </div>
-    </fieldset>
-  </div>
+<Wrapper>
+  <Description
+    content="Renseignez le montant de votre apport personnel. Cela nous permet de
+      calculer la part du financement que vous pouvez couvrir sans emprunter, et
+      d'ajuster le besoin de crédit à solliciter." />
+  <Form onSubmit={handleSubmit}>
+    <div class="fieldset-container">
+      <fieldset class="fr-fieldset">
+        <div class="fr-fieldset__element fr-mb-4w">
+          <Input
+            value={ownContribution}
+            label="Montant de votre apport (€)"
+            labelTooltip="Somme dont vous disposez pour le projet (épargne, donation,
+                aide familiale, etc.)."
+            id="own-contribution"
+            hint="Laissez le champs vide si vous n'avez pas d'apport"
+            error={errors.ownContribution}
+            type="number"
+            placeholder="10 000€"
+            onChange={(e) => {
+              acquisitionSimulatorManger.ownContribution = Number(
+                (e.target as HTMLInputElement).value,
+              );
+            }} />
+        </div>
+      </fieldset>
+    </div>
 
-  {#if nextStep}
     <Actions>
       <Action
         direction="previous"
@@ -77,8 +78,8 @@
         onClick={goToPreviousStep} />
       <Action
         direction="next"
-        label={nextStep.title}
+        label={nextStep?.title as string}
         type="submit" />
     </Actions>
-  {/if}
-</Form>
+  </Form>
+</Wrapper>
