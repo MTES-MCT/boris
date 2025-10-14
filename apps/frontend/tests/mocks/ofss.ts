59b2b2362cd7e078a7056a717b473e20f9709f4f
fuuuzz <benoit.paquier@ik.me>
2025-10-14 14:57:42 +0200

    specs: fix frontend unit tests

--- apps/frontend/tests/mocks/ofss.ts
+++ apps/frontend/tests/mocks/ofss.ts
@@ -107,6 +107,7 @@ export const expectedResult = [
         websiteUrl: null,
         phone: null,
         email: null,
+        producesBrs: null,
         departements: [
           {
             id: '634ab950-c60b-4021-b865-d0941bf613fd',
@@ -141,6 +142,7 @@ export const expectedResult = [
         websiteUrl: 'https://www.foncier-cooperatif-malouin.coop/',
         phone: null,
         email: 'contact.fcm@keredes.coop',
+        producesBrs: null,
         departements: [
           {
             id: '8fb92e67-2a0b-4745-8b1e-e0a76e1e84d2',
@@ -162,6 +164,7 @@ export const expectedResult = [
         websiteUrl: null,
         phone: '06 50 59 31 05',
         email: 'contact@ofs-fst.fr',
+        producesBrs: null,
         departements: [],
         regions: [
           {
@@ -183,6 +186,7 @@ export const expectedResult = [
         websiteUrl: null,
         phone: null,
         email: null,
+        producesBrs: null,
         departements: [],
         regions: [
           {
