0bafb96fec1f5aee9a9786688f866b035a911570
fuuuzz <benoit.paquier@ik.me>
2025-10-16 15:22:25 +0200

    feat: proximity section

---
+++ apps/frontend/src/lib/components/common/ShadowedBox.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  type Props = {
+    children: Snippet;
+  };
+
+  const { children }: Props = $props();
+</script>
+
+<section
+  class="fr-container"
+  id="plafonds-de-ressources">
+  <div class="wrapper">
+    <div class="blue-overlay rounded-box-lg"></div>
+    <div class="content rounded-box-lg background-color-white">
+      {@render children()}
+    </div>
+  </div>
+</section>
+
+<style lang="postcss">
+  .wrapper {
+    position: relative;
+    max-width: 588px;
+    margin: var(--15w) auto var(--10w) auto;
+
+    &::before {
+      content: '';
+      position: absolute;
+      bottom: 100%;
+      left: calc(50% - 2px);
+      height: var(--15w);
+      width: var(--path-thickness);
+      background-color: var(--color-blue-primary);
+    }
+  }
+
+  .content {
+    padding: 24px;
+    z-index: 2;
+    position: relative;
+  }
+
+  @media (--sm-viewport) {
+    .blue-overlay {
+      width: 100%;
+      max-width: 588px;
+      height: 100%;
+      position: absolute;
+      bottom: var(--3w);
+      right: var(--3w);
+      background-color: var(--color-teal-light);
+      z-index: 1;
+    }
+  }
+</style>
---
+++ apps/frontend/src/lib/components/pages/organisme-de-foncier-solidaire/Proximity.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import BoxBlueOverlayed from '$components/common/ShadowedBox.svelte';
+  import Alert from '$components/common/Alert.svelte';
+</script>
+
+<BoxBlueOverlayed>
+  <h2>Un acteur de proximité</h2>
+  <p>
+    On dénombre plus de 155 organismes de foncier solidaire (OFS) agréés au 30
+    Janvier 2025. Ils sont répartis sur le territoire: on trouve des organismes
+    de foncier solidaire (OFS) dans toutes les régions de France métropolitaine.
+    Des OFS sont également agréés en Martinique, Guyane…
+  </p>
+  <p>
+    Certains couvrent une <b>commune ou une métropole</b>
+    , d'autres ont un
+    <b>rayonnement régional ou inter-régional.</b>
+  </p>
+
+  <Alert
+    type="info"
+    title="Vous cherchez un logement en BRS ?">
+    <p>
+      <a
+        href="/logements-brs-disponibles"
+        class="fr-link">
+        Trouver un logement près de chez vous
+      </a>
+      en consultant la liste des sites qui proposent des logements en BRS à vendre.
+    </p>
+  </Alert>
+</BoxBlueOverlayed>
