d0d0b599c4354f52aeb3caee7538aafac106cab8
fuuuzz <benoit.paquier@ik.me>
2025-11-05 17:57:28 +0100

    refacto: ShadowedBox component

--- apps/frontend/src/lib/components/common/ShadowedBox.svelte
+++ apps/frontend/src/lib/components/common/ShadowedBox.svelte
@@ -13,53 +13,17 @@
   class="fr-container"
   id="plafonds-de-ressources">
   <div
-    class="wrapper"
-    class:wrapper--path-hidden={hidePath}>
-    <div class="blue-overlay rounded-box-lg bg-teal-light"></div>
-    <div class="content rounded-box-lg bg-white">
+    class={`
+      relative max-w-[588px] mx-auto mt-30 mb-20
+      before:content-[''] before:absolute before:bottom-[100%] before:left-1/2 before:h-30 before:w-(--path-thickness) before:bg-blue-primary
+    `}
+    class:before:hidden={hidePath}>
+    <div
+      class="blue-overlay hidden md:block absolute w-full max-w-[588px] h-full bottom-[24px] right-[24px] z-[1] rounded-box-lg bg-teal-light">
+    </div>
+
+    <div class="relative p-6 z-[2] rounded-box-lg bg-white">
       {@render children()}
     </div>
   </div>
 </section>
-
-<style lang="postcss">
-  .wrapper {
-    position: relative;
-    max-width: 588px;
-    margin: var(--15w) auto var(--10w) auto;
-
-    &::before {
-      content: '';
-      position: absolute;
-      bottom: 100%;
-      left: calc(50% - 2px);
-      height: var(--15w);
-      width: var(--path-thickness);
-      background-color: var(--color-blue-primary);
-    }
-  }
-
-  .wrapper--path-hidden {
-    &::before {
-      display: none;
-    }
-  }
-
-  .content {
-    padding: 24px;
-    z-index: 2;
-    position: relative;
-  }
-
-  @media (--sm-viewport) {
-    .blue-overlay {
-      width: 100%;
-      max-width: 588px;
-      height: 100%;
-      position: absolute;
-      bottom: var(--3w);
-      right: var(--3w);
-      z-index: 1;
-    }
-  }
-</style>
