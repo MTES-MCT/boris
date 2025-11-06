b50339c2158729fde42eac181bdd81c84298fb36
fuuuzz <benoit.paquier@ik.me>
2025-11-06 15:48:02 +0100

    refacto: CalloutPanel component

--- apps/frontend/src/lib/components/common/StepPage/Hero.svelte
+++ apps/frontend/src/lib/components/common/StepPage/Hero.svelte
@@ -7,7 +7,7 @@
   const { title, children }: Props = $props();
 </script>
 
-<div class="fr-container text-center !max-w-[640px] py-16 md:py-32">
+<div class="fr-container text-center !max-w-[640px] py-16 md:pt-32 md:pt-24">
   <h1>{title}</h1>
   {@render children()}
 </div>
--- apps/frontend/src/lib/components/pages/organismes-de-foncier-solidaire/Synthesis.svelte
+++ apps/frontend/src/lib/components/pages/organismes-de-foncier-solidaire/Synthesis.svelte
@@ -5,51 +5,49 @@
   import Section from '$components/common/Section.svelte';
 </script>
 
-<Section>
-  <CalloutPanel
-    title="En résumé"
-    mascotte="MascotteCelebrating">
-    <div class="fr-col-12 table-container">
-      <Table
-        multiline
-        theads={["Rôle de l'OFS", "Pourquoi c'est important"]}
-        tbodies={[
-          [
-            'Acquiert des terrains et en partage la propriété avec le ménage',
-            'Réduit le coût du logement',
-          ],
-          [
-            "Supervise les règles d'éligibilité et de revente, en accord avec la réglementation existante.",
-            'Garantit la vocation sociale du dispositif',
-          ],
-          [
-            'Perçoit une redevance mensuelle par les différents ménages propriétaires successifs du logement.',
-            "Permet à l'OFS de rembourser son investissement initial correspondant à la baisse du prix du logement qu'il assume lors de la première vente.",
-          ],
-        ]} />
-    </div>
-    <div class="content">
-      <p>
-        Vous avez encore des questions sur les OFS ou sur la manière dont
-        fonctionne le Bail Réel Solidaire ? <a
-          href="/tout-savoir-sur-le-bail-reel-solidaire-brs"
-          class="fr-link">
-          BoRiS vous accompagne pas à pas
-        </a>
-        pour mieux comprendre ce dispositif d'accession sociale à la propriété et
-        faire les bons choix pour votre projet immobilier.
-      </p>
-      <p>
-        Votre OFS n'est pas listé ici ? <a
-          href="/nous-contacter"
-          class="fr-link">
-          Contactez notre équipe
-        </a>
-        du ministère du logement.
-      </p>
-    </div>
-  </CalloutPanel>
-</Section>
+<CalloutPanel
+  title="En résumé"
+  mascotte="MascotteCelebrating">
+  <div class="fr-col-12 table-container">
+    <Table
+      multiline
+      theads={["Rôle de l'OFS", "Pourquoi c'est important"]}
+      tbodies={[
+        [
+          'Acquiert des terrains et en partage la propriété avec le ménage',
+          'Réduit le coût du logement',
+        ],
+        [
+          "Supervise les règles d'éligibilité et de revente, en accord avec la réglementation existante.",
+          'Garantit la vocation sociale du dispositif',
+        ],
+        [
+          'Perçoit une redevance mensuelle par les différents ménages propriétaires successifs du logement.',
+          "Permet à l'OFS de rembourser son investissement initial correspondant à la baisse du prix du logement qu'il assume lors de la première vente.",
+        ],
+      ]} />
+  </div>
+  <div class="content">
+    <p>
+      Vous avez encore des questions sur les OFS ou sur la manière dont
+      fonctionne le Bail Réel Solidaire ? <a
+        href="/tout-savoir-sur-le-bail-reel-solidaire-brs"
+        class="fr-link">
+        BoRiS vous accompagne pas à pas
+      </a>
+      pour mieux comprendre ce dispositif d'accession sociale à la propriété et faire
+      les bons choix pour votre projet immobilier.
+    </p>
+    <p>
+      Votre OFS n'est pas listé ici ? <a
+        href="/nous-contacter"
+        class="fr-link">
+        Contactez notre équipe
+      </a>
+      du ministère du logement.
+    </p>
+  </div>
+</CalloutPanel>
 
 <style lang="postcss">
   .table-container {
