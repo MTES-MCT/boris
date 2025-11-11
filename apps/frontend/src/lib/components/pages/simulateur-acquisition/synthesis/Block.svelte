4fd5280887510af7b844d2d88560bfa8fba5dd2d
fuuuzz <benoit.paquier@ik.me>
2025-11-11 12:03:58 +0100

    refacto: Block component

--- apps/frontend/src/lib/components/pages/simulateur-acquisition/synthesis/Block.svelte
+++ apps/frontend/src/lib/components/pages/simulateur-acquisition/synthesis/Block.svelte
@@ -10,26 +10,7 @@
 </script>
 
 <div
-  class="element"
+  class="element rounded-tl-lg rounded-tr-lg border-2 border-(--background-contrast-info) p-2 xs:p-4 shadow-md"
   class:fr-mb-4w={!isLast}>
   {@render children()}
 </div>
-
-<style lang="postcss">
-  .element {
-    border-top-left-radius: var(--border-radius-sm);
-    border-top-right-radius: var(--border-radius-sm);
-    border: solid 2px var(--background-contrast-info);
-    padding: 0.5rem;
-    box-shadow: 0 3px 6px rgba(134, 144, 162, 0.2);
-
-    @media (--xxs-viewport) {
-      padding: 1rem;
-    }
-
-    @media (--md-viewport) {
-      padding-inline: 1.5rem;
-      padding-block: 1rem;
-    }
-  }
-</style>
