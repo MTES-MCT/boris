a7f119effd1b50f0c916ff7f54d6d76ffedc75b9
fuuuzz <benoit.paquier@ik.me>
2025-11-12 16:55:11 +0100

    feat: create landbot_customer table

---
+++ apps/backend/src/infrastructure/landbot-customer/landbot-customer.entity.ts
@@ -0,0 +1,59 @@
+import {
+  LandbotBrsKnowledge,
+  LandbotCustomerInterface,
+  LandbotEligibility,
+  LandbotRealEstateSituation,
+} from 'src/domain/landbot-customer/landbot-customer.interface';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { DepartementEntity } from '../departement/departement.entity';
+
+@Entity('landbot_customer')
+export class LandbotCustomerEntity implements LandbotCustomerInterface {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column({ type: 'date', nullable: false })
+  public date: Date;
+
+  @ManyToOne(
+    () => DepartementEntity,
+    (departement) => departement.landbotCustomers,
+  )
+  public departement: DepartementEntity;
+
+  @Column({ type: 'enum', enum: LandbotEligibility })
+  public eligibility?: LandbotEligibility;
+
+  @Column({ type: 'enum', enum: LandbotBrsKnowledge })
+  public brsKnowledge?: LandbotBrsKnowledge;
+
+  @Column({ type: 'enum', enum: LandbotRealEstateSituation })
+  public realEstateSituation?: LandbotRealEstateSituation;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  constructor(
+    date: Date,
+    departement: DepartementEntity,
+    eligibility?: LandbotEligibility,
+    brsKnowledge?: LandbotBrsKnowledge,
+    realEstateSituation?: LandbotRealEstateSituation,
+  ) {
+    this.date = date;
+    this.departement = departement;
+    this.eligibility = eligibility;
+    this.brsKnowledge = brsKnowledge;
+    this.realEstateSituation = realEstateSituation;
+  }
+}
