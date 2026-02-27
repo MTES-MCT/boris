af9c7b3678f633a4d44cf0ea962f3c78fbf16324
fuuuzz <benoit.paquier@ik.me>
2026-02-27 12:16:03 +0100

    specs: fix

--- apps/backend/src/infrastructure/eligibility-simulation/dtos/create.dto.ts
+++ apps/backend/src/infrastructure/eligibility-simulation/dtos/create.dto.ts
@@ -7,6 +7,7 @@ import {
   IsNumber,
   IsOptional,
   IsPositive,
+  Min,
 } from 'class-validator';
 
 export class CreateEligibilitySimulationDTO {
@@ -32,6 +33,7 @@ export class CreateEligibilitySimulationDTO {
   @ApiProperty()
   @IsNumber()
   @IsOptional()
+  @Min(0)
   public dependantsAmount?: number;
 
   @ApiProperty()
