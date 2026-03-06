57c8551d49eb6724eb01d2e7acc7068ae7f683ee
Elhaj-Mashkour <azizmashkour@gmail.com>
2026-03-06 13:59:59 +0100

    fix: add lazy loading for images to improve their loading speed

--- apps/frontend/src/routes/action-logement/+page.svelte
+++ apps/frontend/src/routes/action-logement/+page.svelte
@@ -26,6 +26,7 @@
     class="aspect-video"
     width="100%"
     src="https://www.youtube.com/embed/w3J98JlSa9o?si=I4T62YZJbEKhTE6X"
+    loading="lazy"
     title="YouTube video player"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
