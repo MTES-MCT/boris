8ca3814d125c8f74d9f45cd7323bbc327289bf39
Riron <orioncharlier@gmail.com>
2026-04-10 18:22:06 +0200

    Revert label changes

--- apps/frontend/src/lib/utils/eligibility-simulator.ts
+++ apps/frontend/src/lib/utils/eligibility-simulator.ts
@@ -403,16 +403,16 @@ export const stepsContent = {
   eligibility: {
     zoneAandAbis: {
       title:
-        'Vous êtes éligible au dispositif du BRS <u>dans les zones A et Abis</u>.',
+        'Vous êtes éligible au dispositif du BRS <u>en zone A et Abis</u>.',
       titleOwner:
-        'Vous êtes éligible au dispositif du BRS <u>dans les zones A et Abis</u>, sous certaines conditions.',
+        'Vous êtes éligible au dispositif du BRS <u>en zone A et Abis</u>, sous certaines conditions.',
       content: `<p><a class="fr-link" href="/simulateur-eligibilite#plafonds-de-ressources" />Plus d'informations sur les plafonds de ressources.</a></p>`,
     },
     zoneB1: {
       title:
-        'Vous êtes éligible au dispositif du BRS <u>dans les zones A, Abis et B1</u>.',
+        'Vous êtes éligible au dispositif du BRS <u>en zone tendue (A, Abis et B1)</u>.',
       titleOwner:
-        'Vous êtes éligible au dispositif du BRS <u>dans les zones A, Abis et B1</u>, sous certaines conditions.',
+        'Vous êtes éligible au dispositif du BRS <u>en zone tendue (A, Abis et B1)</u>, sous certaines conditions.',
       content: `<p><a class="fr-link" href="/simulateur-eligibilite#plafonds-de-ressources" />Plus d'informations sur les plafonds de ressources.</a></p>`,
     },
     zoneB2andC: {
