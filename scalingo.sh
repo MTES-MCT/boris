b1a78bf8376c8073378b8676327d399592493171
Riron <orioncharlier@gmail.com>
2026-05-16 19:43:49 +0200

    root build

---
+++ scalingo.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+ROOT_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
+
+echo "Building app for $APP"
+
+if echo "$APP" | grep -o 'backend'; then
+    npm run build -w @boris/backend
+    echo "Running migrate for $APP"
+    npm run migration:migrate -w @boris/backend
+
+elif echo "$APP" | grep -q 'frontend'; then
+    npm run build -w @boris/frontend
+
+elif echo "$APP" | grep -q 'ofs-portal'; then
+    npm run build -w @boris/ofs-portal
+fi
